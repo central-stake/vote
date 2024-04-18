aaead197953a4c0cdba326a465c1084f08bd89a7
jpiraguha <jiraguha@gmail.com>
2024-04-18 16:40:53 +0200

    inject public info

--- app/firebase/config.ts
+++ app/firebase/config.ts
@@ -2,14 +2,14 @@ import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    dataBaseUrl: process.env.NEXT_PUBLIC_FIREBASE_DATA_BASE_URL,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCGOZHgBttP9JRIUfIMOr7lK20b-RmIbsE",
+    authDomain: "new-vote-be.firebaseapp.com",
+    projectId: "new-vote-be",
+    storageBucket: "https://new-vote-be-default-rtdb.europe-west1.firebasedatabase.app",
+    messagingSenderId: "new-vote-be.appspot.com",
+    dataBaseUrl: "443136024646",
+    appId: "1:443136024646:web:29ed7d0ad46884f2696a47",
+    measurementId: "G-4YC6VHL1DT",
 };
 
 // Initialize Firebase
