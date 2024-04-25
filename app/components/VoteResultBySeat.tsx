20641e6a1f2a7a3b0a3627666046135bac9e9d8c
Nicolas Jacquet <nicolas.jacquet@risecard.eu>
2024-04-25 08:23:04 +0200

    update vote by seat file

--- app/components/VoteResultBySeat.tsx
+++ app/components/VoteResultBySeat.tsx
@@ -1,4 +1,6 @@
 // @ts-nocheck
+'use client'
+
 import { Doughnut } from 'react-chartjs-2';
 import {
   Chart as ChartJS,
@@ -11,6 +13,9 @@ import {
   Legend,
 } from 'chart.js';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { extractColorsAndLabels, lightenColor } from '@/lib/utils';
+import candidateGroup from '@/lib/candidate-group';
+import { useEffect, useState } from 'react';
 
 ChartJS.register(
   CategoryScale,
@@ -22,124 +27,107 @@ ChartJS.register(
   ChartDataLabels,
   Legend
 );
-// const isMobile = window.innerWidth < 768;
-const isMobile = false;
-function lightenColor(color, factor) {
-  // Remove the "#" symbol if it exists
-  color = color.replace(/^#/, '');
 
-  // Parse the color as a hexadecimal number
-  const num = parseInt(color, 16);
-
-  // Extract the RGB components
-  let red = (num >> 16) & 255;
-  let green = (num >> 8) & 255;
-  let blue = num & 255;
-
-  // Calculate the new RGB values by increasing them
-  // You can adjust the factor to control the lightness
-  red = Math.min(255, red + factor);
-  green = Math.min(255, green + factor);
-  blue = Math.min(255, blue + factor);
-
-  // Convert the new RGB values back to hexadecimal
-  const newColor = `#${(blue | (green << 8) | (red << 16)).toString(16).padStart(6, '0')}`;
-
-  return newColor;
-}
-
-const options = {
-  plugins: {
-    datalabels: {
-      color: '#E5E5E5',
-      textAlign: 'center',
-      anchor: 'bottom',
-     // align: 'bottom',
-      font: {
-        weight: 'bold'
-      },
-      display: !isMobile,
-      formatter: (value, context) => {
-        return context.chart.data.labels[context.dataIndex] +'\n '+ value +'';
-      }
-    },
-    legend: {
-      display: true // This would hide the default legend
-    },
-    tooltip: {
-      callbacks: {
-        label: function(context) {
-          let label = context.label || '';
-          if (label) {
-            label += ': ';
-          }
-          if (context.parsed !== null) {
-            label += context.parsed + ' seats';
-          }
-          return label;
-        }
-      }
-    }
-  },
-  circumference: 180,
-  rotation: -90,
-};
-
-const backC = ['#64DD17', '#FFD600', '#29B6F6', '#2962FF', '#FFAB00', '#FF6D00', '#8D6E63', '#DD2C00', '#C62828', '#B71C1C', '#D50000']
-const backCL = backC.map((x)=> lightenColor(x, 50))
-const data = {
-  labels: ['Ecolo-Groen', 'DéFI', 'Open VLD', 'MR', 'Vlaams Belang', 'N-VA', 'CD&V', 'cdH', 'PS', 'PVDA/PTB', 'sp.a'],
-  datasets: [{
-    data: [21, 2, 12, 14, 18, 25, 12, 5, 20, 12, 9], // The number of seats per party
-    backgroundColor: backC,
-    borderColor: ['#FFFFFF'],
-    borderWidth: 4,
-    cutout: "40%"
-  },
-  {
-    data: [10 , 20 , 5 , 15 , 7 , 9 , 6 , 8 , 12 , 25 , 23], // The number of seats per party
-    backgroundColor: backCL,
-    borderColor: ['#FFFFFF'],
-    borderWidth: 2,
-    cutout: "30%"
-  }
-]
-};
-
-const centerTextPlugin = {
-  id: 'centerText',
-  beforeDraw: (chart) => {
-    if(!isMobile){
-
-
-      const ctx = chart.ctx;
-      const width = chart.width;
-      const height = chart.height;
-      const total = chart.data.datasets[0].data.reduce((sum, value) => sum + value, 0);
-      const centerX = width / 2;
-      const centerY = height / 2;
-
-      ctx.save();
-      ctx.font = '16px Helvetica';
-      ctx.textAlign = 'center';
-      ctx.textBaseline = 'middle';
-      ctx.fillStyle = '#666666';
-      ctx.font = '64px Helvetica';
-      ctx.fillText('150', centerX, centerY+ centerY/2 - 40);
-      ctx.font = '24px Helvetica';
-      ctx.fillText('Seats', centerX, centerY+ centerY/2 - 5 ); // Adjust the position as needed
-      ctx.font = '24px Helvetica';
-      ctx.fillText('89', centerX -50, centerY + centerY/2 + 20); // Adjust the position as needed
-      ctx.fillText('61', centerX + 50, centerY + centerY/2 + 20);
-      ctx.font = '12px Helvetica';// Adjust the position as needed
-      ctx.fillText('Flemish', centerX -50, centerY + centerY/2 + 35); // Adjust the position as needed
-      ctx.fillText('French', centerX + 50, centerY + centerY/2 + 35);
-      ctx.restore();
-    }
-  }
-};
 
 export default function VoteResultBySeat() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    setIsMobile(window.innerWidth < 768);
+  }, []);
+
+  const options = {
+    plugins: {
+      datalabels: {
+        color: '#E5E5E5',
+        textAlign: 'center',
+        anchor: 'bottom' as const,
+        // align: 'bottom',
+        font: {
+          weight: 'bold' as const,
+        },
+        display: !isMobile,
+        formatter: (value: string, context: { chart: { data: { labels: { [x: string]: string; }; }; }; dataIndex: string | number; }) => {
+          return context.chart.data.labels[context.dataIndex] +'\n '+ value +'';
+        }
+      },
+      legend: {
+        display: true // This would hide the default legend
+      },
+      tooltip: {
+        callbacks: {
+          label: function(context: { label: string; parsed: string | null; }) {
+            let label = context.label || '';
+            if (label) {
+              label += ': ';
+            }
+            if (context.parsed !== null) {
+              label += context.parsed + ' seats';
+            }
+            return label;
+          }
+        }
+      }
+    },
+    circumference: 180,
+    rotation: -90,
+  };
+
+  const { optionColors, labels } = extractColorsAndLabels(candidateGroup);
+  const optionColorsLight = optionColors.map((x: string)=> lightenColor(x, 50));
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      // TODO: use data from firebase rtdb
+      data: [21, 2, 12, 14, 18, 25, 12, 5, 20],
+      backgroundColor: optionColors,
+      borderColor: ['#FFFFFF'],
+      borderWidth: 4,
+      cutout: "40%"
+    },
+    {
+      // TODO: use data from firebase rtdb
+      data: [10 , 20 , 5 , 15 , 7 , 9 , 6 , 8 , 12],
+      backgroundColor: optionColorsLight,
+      borderColor: ['#FFFFFF'],
+      borderWidth: 2,
+      cutout: "30%"
+    }
+  ]
+  };
+
+  const centerTextPlugin = {
+    id: 'centerText',
+    beforeDraw: (chart) => {
+      if(!isMobile){
+        const ctx = chart.ctx;
+        const width = chart.width;
+        const height = chart.height;
+        const total = chart.data.datasets[0].data.reduce((sum: any, value: any) => sum + value, 0);
+        const centerX = width / 2;
+        const centerY = height / 2;
+
+        ctx.save();
+        ctx.font = '16px Helvetica';
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillStyle = '#666666';
+        ctx.font = '64px Helvetica';
+        ctx.fillText('150', centerX, centerY+ centerY/2 - 40);
+        ctx.font = '24px Helvetica';
+        ctx.fillText('Seats', centerX, centerY+ centerY/2 - 5 ); // Adjust the position as needed
+        ctx.font = '24px Helvetica';
+        ctx.fillText('89', centerX -50, centerY + centerY/2 + 20); // Adjust the position as needed
+        ctx.fillText('61', centerX + 50, centerY + centerY/2 + 20);
+        ctx.font = '12px Helvetica';// Adjust the position as needed
+        ctx.fillText('Flemish', centerX -50, centerY + centerY/2 + 35); // Adjust the position as needed
+        ctx.fillText('French', centerX + 50, centerY + centerY/2 + 35);
+        ctx.restore();
+      }
+    }
+  };
+
   return (
     <div className='flex items-center justify-center'>
       <Doughnut data={data} options={options} plugins={[ChartDataLabels, centerTextPlugin]} />
